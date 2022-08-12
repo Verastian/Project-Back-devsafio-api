a4350e322c98f1e38c0ebd7ab52c763d9463049c
Matias Seguel <mseguelestay@gmail.com>
2022-08-11 20:39:57 -0400

    fix: fix database config.js file for heroku

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 
-const { NODE_ENV } = process.env;
-
 module.exports = {
   development: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
-    dialectOptions: NODE_ENV !== 'production' ? {} : {
+    dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
